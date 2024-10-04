052348e9e60ca77cc6497a49680b6f77a1a87f27
GeroReyes <geroreyes7789@gmail.com>
2024-10-03 21:30:05 -0600

    Delete: Ventas
    Se elimino la carpeta de ventas

--- ventas/routes/saleRoutes.js
+++
@@ -1,6 +0,0 @@
- const express = require('express');
- const SaleController = require('../controllers/saleController');
-    const router = express.Router();
-    router.post('/sales', SaleController.createSale);
-
-    module.exports = router;
